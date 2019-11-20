ac57240db1d42fd15260e55dbcd6cc9a179ce849
Panos Hatzinikolaou <pjhatzis@gmail.com>
2019-11-20 04:13:29 +0000

    adding the first files of the stretch work

---
+++ merge-arrays.js
@@ -0,0 +1,10 @@
+const merge = function(first, second){
+  for (let i = 0; i < second.length; i++){
+    first.push(second[i]);
+  }
+  return first.sort(function(a, b){ return a - b });
+}
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
