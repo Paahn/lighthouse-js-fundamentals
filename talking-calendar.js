a0a34d16fcdb82984b4eb762efc288e4881a2f5e
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-04-03 21:42:27 +0000

    added skeleton for talking-calendar.js, developing in an EDD manner.

---
+++ talking-calendar.js
@@ -0,0 +1,11 @@
+// We will be given a date as a string (not a Date object).
+// The date will always be formatted as YYYY/MM/DD.
+// We will have to parse the given string and produce a human readable date.
+
+const talkingCalendar = function(date) {
+  
+  return humanReadable;
+
+}
+
+talkingCalendar("2020/04/03");
